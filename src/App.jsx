66cf7e55caca19a46e72c05ffafd39f73a77f242
GustavoBozzano <gustavojorgebozzano@gmail.com>
2024-05-07 09:39:38 +0200

    cambio de Selector

--- src/App.jsx
+++ src/App.jsx
@@ -9,35 +9,37 @@ function App() {
   let jugadaPc = '';
   let jugadaHumano = '';
 
-  const [playHumano, SetPlayHumano] = useState(); // JUGADA HUMANO
-  const [playPc, SetPlayPc] = useState(); // JUGADA PC
-  const [puntosHumano, SetPuntosHumano] = useState(0); // PUNTAJE HUMANO
-  const [puntosPc, SetPuntosPc] = useState(0); // PUNTAJE PC
-  const [noPlay, SetNoPlay] = useState(true); //  DESHABILITAR BOTON DE JUGAR
+  const [playHumano, setPlayHumano] = useState(''); // JUGADA HUMANO
+  const [playPc, setPlayPc] = useState(); // JUGADA PC
+  const [puntosHumano, setPuntosHumano] = useState(0); // PUNTAJE HUMANO
+  const [puntosPc, setPuntosPc] = useState(0); // PUNTAJE PC
+  const [noPlay, setNoPlay] = useState(true); //  DESHABILITAR BOTON DE JUGAR
   const [showModal, setShowModal] = useState(false); // MODAL
-  const [mess, SetMess] = useState(); // MENSAJE EN EL MODAL
+  const [mess, setMess] = useState(); // MENSAJE EN EL MODAL
 
   //  ONCHANGE DEL SELECT  //
   const handleChange = async (e) => {
-    SetNoPlay(false);
+    setNoPlay(false);
+    console.log(playHumano);
   };
 
   //  ONSUBMIT DEL BOTON  //
   const handleSubmit = async (e) => {
     e.preventDefault();
     setShowModal(true);
-    SetMess('');
+    setMess('');
 
     try {
       //      //    jugada seleccionada por el Usuario   //
-      jugadaHumano = document.getElementById('playHuman').value;
-      if (!jugadaHumano) {
-        SetNoPlay(true);
-      } else {
-        SetNoPlay(false);
-      }
+      // jugadaHumano = document.getElementById('playHuman').value;
+      // if (!jugadaHumano) {
+      //   setNoPlay(true);
+      // } else {
+      //   setNoPlay(false);
+      // }
       console.log('la jugada del humano es: ', jugadaHumano);
-      SetPlayHumano(jugadaHumano);
+      console.log('tu jugada es: ', playHumano);
+      // setPlayHumano(jugadaHumano);
 
       //      //    jugada automatica por el Ordenador   //
       let playerPc = Math.floor(Math.random() * 3 + 1);
@@ -55,7 +57,7 @@ function App() {
           break;
       }
       console.log('la jugada del ordenador es: ', jugadaPc);
-      SetPlayPc(jugadaPc);
+      setPlayPc(jugadaPc);
     } catch (error) {
       console.log(error);
       setShowModal(true);
@@ -64,36 +66,36 @@ function App() {
     if (jugadaPc == jugadaHumano) {
       console.log('EMPATE, sigamos participando..!!');
       setShowModal(true);
-      SetMess('EMPATE..!!');
+      setMess('EMPATE..!!');
     }
 
     if (jugadaHumano == 'piedra' && jugadaPc == 'tijera') {
-      SetPuntosHumano((puntosHumano) => puntosHumano + 1);
+      setPuntosHumano((puntosHumano) => puntosHumano + 1);
       console.log(`PUNTO PARA HUMANO`);
-      SetMess('PUNTO para tí..!!');
+      setMess('PUNTO para tí..!!');
     } else if (jugadaHumano == 'papel' && jugadaPc == 'piedra') {
-      SetPuntosHumano((puntosHumano) => puntosHumano + 1);
+      setPuntosHumano((puntosHumano) => puntosHumano + 1);
       console.log(`PUNTO PARA HUMANO`);
-      SetMess('PUNTO para tí..!!');
+      setMess('PUNTO para tí..!!');
     } else if (jugadaHumano == 'tijera' && jugadaPc == 'papel') {
-      SetPuntosHumano((puntosHumano) => puntosHumano + 1);
+      setPuntosHumano((puntosHumano) => puntosHumano + 1);
       console.log(`PUNTO PARA HUMANO`);
-      SetMess('PUNTO para tí..!!');
+      setMess('PUNTO para tí..!!');
     }
     console.log('puntos para Humano: ', puntosHumano);
 
     if (jugadaPc == 'piedra' && jugadaHumano == 'tijera') {
-      SetPuntosPc((puntosPc) => puntosPc + 1);
+      setPuntosPc((puntosPc) => puntosPc + 1);
       console.log(`PUNTO PARA PC`);
-      SetMess('PUNTO para PC..!!');
+      setMess('PUNTO para PC..!!');
     } else if (jugadaPc == 'papel' && jugadaHumano == 'piedra') {
-      SetPuntosPc((puntosPc) => puntosPc + 1);
+      setPuntosPc((puntosPc) => puntosPc + 1);
       console.log(`PUNTO PARA PC`);
-      SetMess('PUNTO para PC..!!');
+      setMess('PUNTO para PC..!!');
     } else if (jugadaPc == 'tijera' && jugadaHumano == 'papel') {
-      SetPuntosPc((puntosPc) => puntosPc + 1);
+      setPuntosPc((puntosPc) => puntosPc + 1);
       console.log(`PUNTO PARA PC`);
-      SetMess('PUNTO para PC..!!');
+      setMess('PUNTO para PC..!!');
     }
     console.log('puntos para PC: ', puntosPc);
   };
@@ -121,10 +123,14 @@ function App() {
             <option value="papel">Papel</option>
             <option value="tijera">Tijera</option>
           </select> */}
-          <SelectorGame SetPlayHumano="jugada" />
+          <SelectorGame onValueSelected={setPlayHumano} />
         </div>
         <div>
-          <button type="submit" id="button" disabled={noPlay}>
+          <button
+            type="submit"
+            id="button"
+            //  disabled={noPlay}
+          >
             JUGADA
           </button>
         </div>
--- src/Selector/index.jsx
+++ src/Selector/index.jsx
@@ -1,63 +1,71 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 import './selector.css';
 
-export function SelectorGame({ jugada }) {
+export function SelectorGame({ onValueSelected }) {
+  const handleRadioChange = (event) => {
+    const selectedValue = event.target.value;
+    onValueSelected(selectedValue);
+    console.log(selectedValue);
+  };
   return (
     <div className="container">
       <div className="radio-tile-group">
-        <div className="input-container">
-          <input
-            id="piedra"
-            className="radio-button"
-            type="radio"
-            name="radio"
-            value="piedra"
-          ></input>
-          <div className="radio-tile">
-            <div className="icon piedra-icon">
-              <img src="../public/Piedra.jpg" alt="Piedra" />
+        <form onSubmit={handleRadioChange}>
+          <div className="input-container">
+            <input
+              id="playHuman"
+              className="radio-button"
+              type="radio"
+              name="radio"
+              value="piedra"
+            ></input>
+            <div className="radio-tile">
+              <div className="icon piedra-icon">
+                <img src="../public/Piedra.jpg" alt="Piedra" />
+              </div>
+              <label htmlFor="walk" className="radio-tile-label">
+                Piedra
+              </label>
             </div>
-            <label htmlFor="walk" className="radio-tile-label">
-              Piedra
-            </label>
           </div>
-        </div>
 
-        <div className="input-container">
-          <input
-            id="papel"
-            className="radio-button"
-            type="radio"
-            name="radio"
-            value="papel"
-          ></input>
-          <div className="radio-tile">
-            <div className="icon papel-icon">
-              <img src="../public/Papel.jpg" alt="Papel" />
+          <div className="input-container">
+            <input
+              id="playHuman"
+              className="radio-button"
+              type="radio"
+              name="radio"
+              value="papel"
+            ></input>
+            <div className="radio-tile">
+              <div className="icon papel-icon">
+                <img src="../public/Papel.jpg" alt="Papel" />
+              </div>
+              <label htmlFor="papel" className="radio-tile-label">
+                Papel
+              </label>
             </div>
-            <label htmlFor="papel" className="radio-tile-label">
-              Papel
-            </label>
           </div>
-        </div>
 
-        <div className="input-container">
-          <input
-            id="tijera"
-            className="radio-button"
-            type="radio"
-            name="radio"
-            value="tijera"
-          ></input>
-          <div className="radio-tile">
-            <div className="icon tijera-icon">
-              <img src="../public/tijera.jpg" alt="Tijera" />
+          <div className="input-container">
+            <input
+              id="playHuman"
+              className="radio-button"
+              type="radio"
+              name="radio"
+              value="tijera"
+            ></input>
+            <div className="radio-tile">
+              <div className="icon tijera-icon">
+                <img src="../public/tijera.jpg" alt="Tijera" />
+              </div>
+              <label htmlFor="tijera" className="radio-tile-label">
+                Tijera
+              </label>
             </div>
-            <label htmlFor="tijera" className="radio-tile-label">
-              Tijera
-            </label>
           </div>
-        </div>
+        </form>
       </div>
     </div>
   );
